62eb1f23780e4320d7ba128a61c7e88fd77e1bf8
peach <ginahenstock7@gmail.com>
2021-12-02 22:02:17 +0200

    Add files via upload
    neatening it up

---
+++ bashOS.py
@@ -0,0 +1,11 @@
+#Directorys
+d_fcache = '~/Desktop/fakeCache'
+d_cache = '/home/be/.config/discord/Cache'
+urlsList = []
+
+
+# moves files from real sicord cache into fake cache to avoid 'cant read IsaDirectoryError'
+Cache = "sudo find " + d_cache + " -type f -exec mv -t " + d_fcache + "/ {} \; && sudo chmod 777 -R " + d_fcache
+
+#shreds cache
+Shred = "sudo find " + d_fcache + " -type f -exec mv -t /home/be/Desktop/shred/ {} \; && sudo ./killerbean" 
