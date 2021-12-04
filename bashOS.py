0896b79a01e1f27734aa3ac2a8b50c661a2079ea
peach <ginahenstock7@gmail.com>
2021-12-04 16:51:50 +0200

    Update bashOS.py

--- bashOS.py
+++ bashOS.py
@@ -12,6 +12,4 @@ urlsList = []
 # moves files from real sicord cache into fake cache to avoid 'cant read IsaDirectoryError'
 Cache = "sudo find " + d_cache + " -type f -exec mv -t " + d_fcache + "/ {} \; && sudo chmod 777 -R " + d_fcache
 
-#shreds cache
-Shred = "sudo find " + d_fcache + " -type f -exec mv -t /home/be/Desktop/shred/ {} \; && sudo ./killerbean" 
 
